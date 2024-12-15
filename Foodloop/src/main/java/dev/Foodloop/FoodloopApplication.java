55154b544fe9e33a16bd60a764bdd4709292061a
Daly Bouazizi <bouazizimohamedali@isimsf.u-sfax.tn>
2024-12-15 18:34:36 +0100

    First commit

---
+++ Foodloop/src/main/java/dev/Foodloop/FoodloopApplication.java
@@ -0,0 +1,13 @@
+package dev.Foodloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodloopApplication.class, args);
+	}
+
+}
---
+++ Foodloop/src/test/java/dev/Foodloop/FoodloopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Foodloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
